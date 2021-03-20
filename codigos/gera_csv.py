02ab48f6d1461f86d04092991ea80818996f1742
Orivaldo Santana <orivaldo@gmail.com>
2021-03-20 19:39:04 -0300

    gerando as notas para a unidade  1

--- codigos/gera_csv.py
+++ codigos/gera_csv.py
@@ -1,3 +1,48 @@
+import processa_notas as pn
 import pandas as pd
+
+# Leitura dos dados das notas das listas do sistema LoP
 notas_dados = pd.read_csv("../dados/lop_exercicios_2021_02_27.csv")
 print(notas_dados.head())
+
+
+# seleciona as coluas de interesse para a unidade 1
+notas_u1 = notas_dados.iloc[:,  [1, 2, 3, 4, 5, 6, 7]]
+# seleciona as coluas de interesse para a unidade 2
+notas_u2 = notas_dados.iloc[:,  [1, 8, 9, 10, 11, 12, 16]]
+# seleciona as coluas de interesse para a unidade 3
+notas_u3 = notas_dados.iloc[:, [1, 13, 14, 15]]
+
+print("\nSeleção de Listas para a Unidade I")
+print(notas_u1.columns)
+
+print("\nSeleção de Listas para a Unidade II")
+print(notas_u2.columns)
+
+print("\nSeleção de Listas para a Unidade III")
+print(notas_u3.columns)
+
+# Calcula as médias
+notas_u1 = pn.calcula_media(notas_u1)
+#notas_u2 = pn.calcula_media(notas_u2)
+#notas_u3 = pn.calcula_media(notas_u3)
+
+print(notas_u1.head())
+
+# Organiza de acordo com as matrículas do SIGAA
+turma_sigaa = pd.read_csv("../dados/turma_03_2020_2.csv")
+
+print(turma_sigaa.head())
+
+padronizada_notas_u1 = pn.padroniza_sigaa(notas_u1, turma_sigaa)
+#padronizada_notas_u2 = pn.padroniza_sigaa(notas_u2, turma_sigaa)
+#padronizada_notas_u3 = pn.padroniza_sigaa(notas_u3, turma_sigaa)
+# remove linhas duplicadas
+padronizada_notas_u1 = padronizada_notas_u1.drop_duplicates()
+#padronizada_notas_u2 = padronizada_notas_u2.drop_duplicates( )
+#padronizada_notas_u3 = padronizada_notas_u3.drop_duplicates()
+
+print(padronizada_notas_u1.head())
+
+padronizada_notas_u1.to_csv(
+    "../saida/notas_uniao_u1_completo.csv", index=False)
---
+++ codigos/processa_notas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def padroniza_sigaa(notas_u, turma_sig):
+    # converte o campo matrícula para inteiro
+    notas_u.loc[:, 'Matrícula'] = notas_u.loc[:, 'Matrícula'].astype(int)
+    # junta dois data-frames pela matrícula
+    notas_uniao_u = pd.merge(turma_sig, notas_u, how='left', on='Matrícula')
+    return notas_uniao_u
+
+
+def calcula_media(notas_u):
+    # divite as notas por 10
+    notas_u_listas = notas_u
+    notas_u_listas.iloc[:, 1:] = notas_u.iloc[:, 1:].div(10)
+    # Calcula a média de cada aluno
+    notas_u_listas.loc[:, 'Média'] = notas_u_listas.iloc[:, 1:].mean(axis=1)
+    return notas_u_listas
